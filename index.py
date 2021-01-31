315cfbe8bf19929e4dc042cfa8bb90d4482daa73
anousoneFS <sone.freestyle7898@gmail.com>
2021-01-31 12:17:49 +0700

    add function name

--- index.py
+++ index.py
@@ -2,3 +2,11 @@ print("hi anousone")
 print("hi anousone")
 print("hi anousone")
 print("hi anousone")
+
+
+def name(first_name, last_name):
+    return "hi:" + first_name + last_name
+
+
+if __name__ == "__main__":
+    print(name("sone", "freestyle"))
