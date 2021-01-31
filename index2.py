bbbb29768f4e8a061c353cdbe8f38dd4abaf1cb4
anousoneFS <sone.freestyle7898@gmail.com>
2021-01-31 15:47:09 +0700

    .

--- index2.py
+++ index2.py
@@ -9,12 +9,6 @@ def name(first_name, last_name):
 
 if __name__ == "__main__":
     print(name("anousone", "worlakoumman"))
-<<<<<<< HEAD
     print(name("sone", "worlakoumman"))
 
     print("hi anousone")
-=======
-
-    print("house")
-    print("house")
->>>>>>> 2108aeca2f11803a0b2ee2e02a10a798ffbda97d
