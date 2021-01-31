ea03569d698fda79f38c220428e2e4c9eeee84d5
anousoneFS <sone.freestyle7898@gmail.com>
2021-01-31 12:24:14 +0700

    add function name haha

--- index2.py
+++ index2.py
@@ -1,3 +1,11 @@
 print("this is another computer")
 print("hahah")
 print("how are you?")
+
+
+def name(first_name, last_name):
+    return "hi:" + first_name + last_name
+
+
+if __name__ == "__main__":
+    print(name("anousone", "worlakoumman"))
