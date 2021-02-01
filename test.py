c61457ba41971618b43ed7fa24ccbe8e86fe1b80
anousoneFS <sone.freestyle7898@gmail.com>
2021-02-01 19:23:12 +0700

    add feature name

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 print("this is test branch")
 print("anousone")
 print("sone freestyle")
+
+
+def name(first_name, last_name):
+    return "hi:" + first_name + last_name
+
+
+print(name("sone", "freestyle"))
